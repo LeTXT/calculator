7d370ccbddde77a05ad2ac325b056d9d7c09c576
LeTXT <leandrogomesdeoliveira2@gmail.com>
2022-02-02 02:32:22 -0300

    change screen

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,13 +1,5 @@
 import React, { useState } from "react"
 
-import {array} from './Array'
-
-import timeBlack from './img/timeBlack.svg'
-import timeWhite from './img/timeWhite.svg'
-
-import divisionBlack from './img/divisionBlack.svg'
-import divisionWhite from './img/divisionWhite.svg'
-
 
 export default function Screen({stateCa, state}) {
     const [screenState, setScreenState] = useState(state)
